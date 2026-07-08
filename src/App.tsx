53832a1ad2bfc50bbcd04753f012d0c599cac562
hilman.wasiandi <hilman.wasiandi@tritronik.com>
2026-07-08 11:24:03 +0700

    CHORE :: add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import './App.css'
+
 import { useState } from 'react'
+import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { ThemeProvider } from './providers/ThemeProvider'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <ThemeProvider>
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
@@ -115,7 +117,7 @@ function App() {
 
       <div className="ticks"></div>
       <section id="spacer"></section>
-    </>
+    </ThemeProvider>
   )
 }
 
